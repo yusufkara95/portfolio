29d3536f56f4af1cfaa3c51ba15e31932bc8faea
Yusuf Kara <yusufkara@gmx.de>
2024-01-22 12:54:49 +0100

    Fix

--- src/components/ProfileImage/index.jsx
+++ src/components/ProfileImage/index.jsx
@@ -4,7 +4,7 @@ import profileImage from '../../assets/profile.jpeg';
 function ProfileImage() {
   return (
     <div>
-      <img src={profileImage} style={{ borderRadius: '50%', width: '256px', height: '256px', borderColor: '#435C5C', border: '5px solid #435C5C' }} />
+      <img src={profileImage} alt='ProfileImage' style={{ borderRadius: '50%', width: '256px', height: '256px', borderColor: '#435C5C', border: '5px solid #435C5C' }} />
     </div>
   );
 }
